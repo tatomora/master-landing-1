552ce3b719e80a98ba746f59abc7c7765b7b134b
Jesus Quevedo <jesus.quevedo@morningstar.com>
2021-07-28 22:50:01 -0500

    working in travel section

---
+++ js/app.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-new */
+
+$(".glider").slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
